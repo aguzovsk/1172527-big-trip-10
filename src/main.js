884e67e09d32c124df75cc6c27e112ecf5d8f949
anton Guzovskiy <antonguzowski@gmail.com>
2019-12-05 20:13:34 +0100

    module1-task3 corrected

--- src/main.js
+++ src/main.js
@@ -79,7 +79,7 @@ const createSortedTemplate = () => {
   );
 };
 
-/*const createNewEventTemplate = () => {
+/* const createNewEventTemplate = () => {
   return (
     `<form class="trip-events__item  event  event--edit" action="#" method="post">
       <header class="event__header">
